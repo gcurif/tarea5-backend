db2eb139eb1850e5bed5fe4f1d2be88e411e9ae2
Gustavo Curifuta <gustavo.curifuta@usach.cl>
2024-04-11 12:38:15 -0400

    feat:update db creds

--- database_manager.py
+++ database_manager.py
@@ -4,7 +4,7 @@ def get_db():
     db = connect(
         db="pafcat-static",
         host="viaduct.proxy.rlwy.net",
-        port=28844,
+        port=23766,
         username="leinas",
         password="pafcat.2024"
     )
