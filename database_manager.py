3d4b20aae7198ce57854683ce01a7b69591a791e
Gustavo Curifuta <gustavo.curifuta@usach.cl>
2024-04-11 12:24:39 -0400

    feat: update base de datos

--- database_manager.py
+++ database_manager.py
@@ -2,8 +2,8 @@ from mongoengine import connect
 
 def get_db():
     db = connect(
-        db="pafcat-dev",
-        host="monorail.proxy.rlwy.net",
+        db="pafcat-static",
+        host="viaduct.proxy.rlwy.net",
         port=28844,
         username="leinas",
         password="pafcat.2024"
